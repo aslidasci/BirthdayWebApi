e41d5584349dc35f5b998d9f4107343fa8db06bf
asli dasci <asli.dsc.20.09@gmail.com>
2019-08-30 10:58:25 +0300

    version1

---
+++ Controllers/DavetiyeController.cs
@@ -0,0 +1,33 @@
+﻿using BirthdayApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BirthdayApp.Controllers
+{
+    public class DavetiyeController : ApiController
+    {
+        
+        public IEnumerable<DavetiyeModel> GetKatılanlar()
+        {
+            return Veritabani.Liste.Where(i => i.KatilmaDurumu == true);
+        }
+        [HttpGet]
+        public IEnumerable<DavetiyeModel> GetKatılmayanlar()
+        {
+            return Veritabani.Liste.Where(i => i.KatilmaDurumu == false);
+        }
+        public void PostEkle(DavetiyeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Veritabani.Add(model);
+
+            }
+
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using BirthdayApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DavetiyeFormu()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DavetiyeFormu(DavetiyeModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                Veritabani.Add(model);
+                return View("Thanks", model);
+            }
+
+            return View(model);
+        }
+        [ChildActionOnly]
+        public ActionResult Katilanlar()
+        {
+            var katilanlar = Veritabani.Liste.Where(i => i.KatilmaDurumu == true);
+            return PartialView(katilanlar);
+        }
+    }
+}
